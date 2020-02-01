c8163ab649a5c69673238d7a2094211220911726
Littlebath <49510476+Littlebath@users.noreply.github.com>
2020-02-01 18:01:39 +0000

    Revert "Revert "Added gallery scene""
    This reverts commit 49b7344c3471a048a21be921a32d9e5999174257.

---
+++ Assets/Scripts/FindImage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class FindImage : MonoBehaviour
+{
+    public int ID = 1;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        WWW www = new WWW("file:///" + Data.filename);
+        Debug.Log ("file:///" + Data.filename);
+        while (!www.isDone)
+            yield return null;
+        GameObject image = GameObject.Find("RawImage");
+        image.GetComponent<RawImage>().texture = www.texture;
+    }
+
+    public void SaveScreenshot()
+    {
+        FileUtil.MoveFileOrDirectory(Data.filename, "C:/GGJ20_Screeshots/" + Data.filename);
+    }
+}
