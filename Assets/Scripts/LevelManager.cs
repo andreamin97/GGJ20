6850c1f98a30e7352fa14df04c24b8eb69e4c086
Littlebath <49510476+Littlebath@users.noreply.github.com>
2020-02-01 14:43:56 +0000

    Added randomzied drawing canvas

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Sprite[] images;
+    public GameObject drawingCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite randomImage = images[Random.Range(0, images.Length)];
+        drawingCanvas.GetComponent<SpriteRenderer>().sprite = randomImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
